dc54cce7a430f5e761cb0b9b9fd79b5cad74fb06
Akinsete Ayomide <Akinseteayomide247@gmail.com>
2026-02-11 19:05:50 +0100

    intial commit:User

---
+++ index.js
@@ -0,0 +1,8 @@
+const user ={
+    name:'Akinsete Ayomide',
+    age : 24,
+    language: 'js',
+};
+console.log(user.name);
+console.log(user.age);
+console.log(user.language);
--- intro.js
+++ intro.js
@@ -1,1 +1,10 @@
-console.log ('hello' , 'world!');
+const user ={
+    name:'Akinsete Ayomide',
+    age:24,
+    language:'js',
+};
+console.log (user.name);
+console.log(user.age);
+console.log(user.language);
+
+
